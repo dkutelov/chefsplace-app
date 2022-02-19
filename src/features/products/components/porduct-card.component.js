6c9e9c19dc06ec92ad1675104f35ed689534844f
dkutelov <dkutelov@digitals.bg>
2022-02-19 17:46:51 +0200

    Styled components added

--- src/features/products/components/porduct-card.component.js
+++ src/features/products/components/porduct-card.component.js
@@ -1,11 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Card } from "react-native-paper";
+import styled from "styled-components";
+
+const ProductCardWrapper = styled(Card)`
+  background-color: red;
+`;
 
 export const ProductCard = () => {
   return (
-    <Card>
+    <ProductCardWrapper elevation={5}>
       <Card.Title title="Card Title" />
-    </Card>
+    </ProductCardWrapper>
   );
 };
