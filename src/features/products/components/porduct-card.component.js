264faab8745bacd62b284df251e0888cd38b7ac8
dkutelov <dkutelov@digitals.bg>
2022-02-19 17:32:00 +0200

    React native paper added, simple card test

---
+++ src/features/products/components/porduct-card.component.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Card } from "react-native-paper";
+
+export const ProductCard = () => {
+  return (
+    <Card>
+      <Card.Title title="Card Title" />
+    </Card>
+  );
+};
