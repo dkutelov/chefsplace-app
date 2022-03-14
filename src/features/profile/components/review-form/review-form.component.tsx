ac1d8860f222c6c2b949c8bdb401834ff2c9aeb2
dkutelov <dkutelov@digitals.bg>
2022-03-14 10:07:16 +0200

    Basic Formik

---
+++ src/features/profile/components/review-form/review-form.component.tsx
@@ -0,0 +1,42 @@
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import React from "react";
+import { Formik } from "formik";
+
+export const ReviewForm = () => {
+  return (
+    <View>
+      <Formik
+        initialValues={{ title: "", body: "", rating: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <TextInput
+              placeholder="Review product"
+              onChangeText={props.handleChange("title")}
+              value={props.values.title}
+            />
+            <TextInput
+              multiline
+              placeholder="Review body"
+              onChangeText={props.handleChange("body")}
+              value={props.values.body}
+            />
+            <TextInput
+              placeholder="Rating (1-5)"
+              onChangeText={props.handleChange("rating")}
+              value={props.values.rating}
+            />
+            <Button
+              title="Submit"
+              color="maroon"
+              onPress={props.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
--- src/features/profile/screens/index.tsx
+++ src/features/profile/screens/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
+import { ReviewForm } from "../components/review-form/review-form.component";
 
 export const ProfileScreen = () => {
   return (
     <View>
       <Text>Profile</Text>
+      <ReviewForm />
     </View>
   );
 };
