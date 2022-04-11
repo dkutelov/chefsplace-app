1b7530adb5f409f79369eb7917f5b45ea47d41cc
dkutelov <dkutelov@digitals.bg>
2022-04-11 11:53:09 +0300

    Delete user cloud function

--- functions/src/user/index.js
+++ functions/src/user/index.js
@@ -1,6 +1,28 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-exports.hiUser = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello from user!", { structuredData: true });
-  response.send("Hi, User");
+admin.initializeApp();
+
+const db = admin.firestore();
+const storage = admin.storage().bucket();
+const auth = admin.auth();
+
+// Always terminate with send, end or redirect! Otherwise the functio will keep running.
+exports.deleteUser = functions.https.onRequest(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await auth.getUserByEmail(email).catch((error) => {
+    console.log(error);
+    res.status(500).send({ message: "Unable to find user!" });
+  });
+
+  auth
+    .deleteUser(user.uid)
+    .then(() => {
+      res.status(200).send("User susccesfully deleted!");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
 });
